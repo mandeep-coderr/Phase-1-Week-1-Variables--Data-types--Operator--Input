5c0f336ef1f54112f31048f7de496e31c5f5f8f3
Mandeep <mandeep.singh.build@gmail.com>
2026-05-14 15:06:49 +0530

    Add arithmetic and comparison operators examples
    Created two new Python files demonstrating arithmetic and comparison operators. The first file includes basic operations, operator precedence, and augmented assignments. The second file provides practice tasks for arithmetic operations and comparisons, including area calculations and modulo checks.

---
+++ 2. Arithmetic and comparison operators/Arithmetic_operators.py
@@ -0,0 +1,37 @@
+#Arithmetic operators
+print (50 + 8)        #Add
+
+print(500 - 75)       #subtraction
+
+print(8 * 20)         #multiplication
+
+print(16 / 4)         #Regular Division (/) This will show us decimal numbers 25.457
+
+print(15 // 4)        #Floor Division (//)  This erase decimal point number only 25
+
+print(25 % 7)         #Modulo (Remainder) that remained at the end after calculate is 4
+
+print(9 ** 4)         #Power means multiply 9 four time. 9 * 9 * 9 * 9
+
+#Comparision Operators
+print(10 == 20)       # "Equal To" (==) 10 is not equal to 20 answer is False
+
+print(10 != 10)       # "Not Equal To" But is is opposite if both are equal then false
+
+print(60 > 50)        # "> Greater Than"
+
+print(100 < 50)       # "< Less Than"
+
+print(18 >= 18)       # Greater than or Equal to
+
+print(14 <= 20)       # Less than or Equal to
+
+#Operator Precedence (BODMAS) bodmas follow the rule 1st mulplication, divide later - , +
+print(12 * 4 + 13)    # So its answer would be 61
+
+print(12 * (4 + 13))       # Brackets when we apply bracket computer ignore bodmas it follow brackets
+
+#Augmented Assignment += , -= , *= , /=
+health = 100
+health -= 20          #When we want to score Add, sbtract, multiply 
+print(health)         #A short way to change a variable’s value without writing the variable name again
---
+++ 2. Arithmetic and comparison operators/practice_task_arithmetic_operators.py
@@ -0,0 +1,34 @@
+a = 17
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+num = 18
+print(num % 2)         #modulo(To check out if its odd or even number)
+num = 7
+print(num % 2)         #modulo(To check out if its even or odd number)
+
+length= 12
+width = 7
+print(length * width)             #Area Calculate
+print((length + width) * 2)       #Parameter of a rectangle
+
+x = 20
+y = 18
+print(x == y)  
+print(x != y) 
+print(x > y)                      # Comparision Expression
+print(x < y) 
+print(x >= y) 
+print(x <= y) 
+
+price = 100
+price += 18
+price -= 5                        # Use Agumented Assignment += , -=
+print(price)
